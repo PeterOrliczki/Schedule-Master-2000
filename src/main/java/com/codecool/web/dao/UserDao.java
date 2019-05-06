af990d02534f94ed6fd96347f7dfbcaa51c00f96
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-06 22:25:25 +0200

    Added user database functions to dao package DatabaseUserDao and UserDao interface

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -1,10 +1,25 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.model.Role;
 import com.codecool.web.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDao {
+    List<User> findAllUsers() throws SQLException;
 
+    User addUser(String name, String email, String password, Role role) throws SQLException;
+
+    User findUserById(int id) throws SQLException;
+
+    User findUserByName(String name) throws SQLException;
+
+    void deleteUserById(int id) throws SQLException;
+
+    void updateUserNameById(String id, String name) throws SQLException;
+
+    void updateUserEmailById(String id, String email) throws SQLException;
+
+    void updateUserPasswordById(String id, String password) throws SQLException;
 }
--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -1,8 +1,10 @@
 package com.codecool.web.dao.database;
 
 import com.codecool.web.dao.UserDao;
+import com.codecool.web.model.Role;
 import com.codecool.web.model.User;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,147 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
         super(connection);
     }
 
-    public List<User> findAll() throws SQLException {
+    @Override
+    public List<User> findAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery((sql)) ) {
+            while (resultSet.next()) {
+                users.add(fetchUser(resultSet));
+            }
+        }
+        return users;
+    }
+
+    @Override
+    public User findUserById(int id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE user_id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
         return null;
+    }
+
+    @Override
+    public User findUserByName(String name) throws SQLException {
+        String sql = "SELECT * FROM users WHERE user_name = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, name);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public User addUser(String name, String email, String password, Role role) throws SQLException {
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO users(name, email, password, role) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, password);
+            statement.setObject(4, role);
+            executeInsert(statement);
+            int id = fetchGeneratedId(statement);
+            connection.commit();
+            return new User(id, name, email, password, role);
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    @Override
+    public void deleteUserById(int id) throws SQLException {
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "DELETE FROM users WHERE user_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setInt(1, id);
+            executeInsert(preparedStatement);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    @Override
+    public void updateUserNameById(String id, String name) throws SQLException {
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "update users set user_name=? where user_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, name);
+            statement.setString(2, id);
+            executeInsert(statement);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    @Override
+    public void updateUserEmailById(String id, String email) throws SQLException {
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "update users set user_email=? where user_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, email);
+            statement.setString(2, id);
+            executeInsert(statement);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    @Override
+    public void updateUserPasswordById(String id, String password) throws SQLException {
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "update users set user_password=? where user_id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1,password);
+            statement.setString(2, id);
+            executeInsert(statement);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
     }
 
 
     private User fetchUser(ResultSet resultSet) throws SQLException {
-        return null;
+        int id = resultSet.getInt("user_id");
+        String name = resultSet.getString("user_name");
+        String email = resultSet.getString("user_email");
+        String password = resultSet.getString("user_password");
+        Role role = (Role)resultSet.getObject("user_role");
+
+        return new User(id, name, email, password, role);
     }
 }
