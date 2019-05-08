dffdec141fe1d58c7125b37df9137d2e7da8b0af
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-08 11:17:32 +0200

    Schedule Service and view Schedule trough servlet are finished

--- src/main/java/com/codecool/web/service/ScheduleService.java
+++ src/main/java/com/codecool/web/service/ScheduleService.java
@@ -8,4 +8,19 @@ import java.util.List;
 
 public interface ScheduleService {
 
+    List<Schedule> findAll() throws SQLException, ServiceException;
+
+    Schedule findByScheduleId(int id) throws SQLException;
+
+    Schedule findByUserId(int id) throws SQLException;
+
+    Schedule addTask(int userId, String scheduleTitle, int scheduleDuration) throws SQLException;
+
+    void deleteByScheduleId(int id) throws SQLException;
+
+    void updateTitleById(int id, String title) throws SQLException;
+
+    void updateDurationById(int id, int duration) throws SQLException;
+
+    void updateVisibilityById(int id, boolean visibility) throws SQLException;
 }
