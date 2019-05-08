717690b6c38313dc02d363cacd5205d0090492d4
PeterOrliczki <peterorliczkiprimary@gmail.com>
2019-05-08 12:39:13 +0200

    Servlet methods for viewing and adding tasks

---
+++ src/main/java/com/codecool/web/servlet/TaskServlet.java
@@ -0,0 +1,61 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.ScheduleDao;
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseScheduleDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Task;
+import com.codecool.web.model.User;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.simple.SimpleTaskService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+
+@WebServlet("/tasks")
+public class TaskServlet extends AbstractServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao, scheduleDao);
+
+            User user = (User) request.getSession().getAttribute("user");
+            List<Task> tasks = taskService.findAllByTaskId(user.getId());
+
+            sendMessage(response, HttpServletResponse.SC_OK, tasks);
+        } catch (SQLException exc) {
+            handleSqlError(response, exc);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao, scheduleDao);
+
+            User user = (User) request.getSession().getAttribute("user");
+
+            int userId = user.getId();
+            String taskTitle = request.getParameter("task-title");
+            String taskContent = request.getParameter("task-content");
+            int taskStart = Integer.valueOf(request.getParameter("task-start"));
+            int taskEnd = Integer.valueOf(request.getParameter("task-end"));
+
+            taskService.addTask(userId, taskTitle, taskContent, taskStart, taskEnd);
+
+        } catch (SQLException exc) {
+            handleSqlError(response, exc);
+        }
+    }
+
+}
