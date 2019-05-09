e34c2321dcc4b1dd25a7f7a14c0513b623e8f7fd
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-09 20:41:50 +0200

    Task editing in progress

--- src/main/java/com/codecool/web/servlet/TasksServlet.java
+++ src/main/java/com/codecool/web/servlet/TasksServlet.java
@@ -4,10 +4,14 @@ import com.codecool.web.dao.ScheduleDao;
 import com.codecool.web.dao.TaskDao;
 import com.codecool.web.dao.database.DatabaseScheduleDao;
 import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Schedule;
 import com.codecool.web.model.Task;
 import com.codecool.web.model.User;
+import com.codecool.web.service.ScheduleService;
 import com.codecool.web.service.TaskService;
+import com.codecool.web.service.simple.SimpleScheduleService;
 import com.codecool.web.service.simple.SimpleTaskService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +25,8 @@ import java.util.List;
 
 @WebServlet("/protected/tasks")
 public class TasksServlet extends AbstractServlet {
+
+    private final ObjectMapper om = new ObjectMapper();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (Connection connection = getConnection(request.getServletContext())) {
@@ -55,6 +61,24 @@ public class TasksServlet extends AbstractServlet {
             sendMessage(response, HttpServletResponse.SC_OK, "Task succesfully added");
         } catch (SQLException exc) {
             handleSqlError(response, exc);
+        }
+    }
+
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao, scheduleDao);
+
+            Task task = om.readValue(req.getInputStream(), Task.class);
+            taskService.updateTitleById(task.getId(), task.getTitle());
+            taskService.updateContentById(task.getId(), task.getContent());
+            taskService.updateStartById(task.getId(), task.getStart());
+            taskService.updateEndById(task.getId(), task.getEnd());
+
+            sendMessage(resp, HttpServletResponse.SC_OK, "Schedule updated.");
+        } catch (SQLException exc) {
+            handleSqlError(resp, exc);
         }
     }
 
