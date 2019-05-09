088e84192d262e145cacc732f531aaf134eabd37
PeterOrliczki <peterorliczkiprimary@gmail.com>
2019-05-09 10:04:09 +0200

    From varchar to integer fix

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS schedule_tasks CASCADE;
-DROP TABLE IF EXISTS tasks CASCADE;
-DROP TABLE IF EXISTS schedules CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS all_audit CASCADE;
-DROP SEQUENCE IF EXISTS counter CASCADE;
+drop table IF EXISTS schedule_tasks CASCADE;
+drop table IF EXISTS tasks CASCADE;
+drop table IF EXISTS schedules CASCADE;
+drop table IF EXISTS users CASCADE;
+drop table IF EXISTS all_audit CASCADE;
+drop sequence IF EXISTS counter CASCADE;
 
-CREATE TABLE users(
+create TABLE users(
 	user_id SERIAL UNIQUE PRIMARY KEY,
 	user_name varchar(16) NOT NULL,
 	user_email varchar(254) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE users(
 	CONSTRAINT user_role_not_empty CHECK (user_role <> '')
 );
 
-CREATE TABLE schedules(
+create TABLE schedules(
 	schedule_id SERIAL UNIQUE PRIMARY KEY,
 	user_id integer,
 	schedule_title varchar(40) NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE schedules(
 	CONSTRAINT schedule_duration_max CHECK (schedule_duration > 0 AND schedule_duration <= 7)
 );
 
-CREATE TABLE tasks(
+create TABLE tasks(
 	task_id SERIAL UNIQUE PRIMARY KEY,
 	user_id integer,
 	task_title text NOT NULL,
@@ -39,24 +39,24 @@ CREATE TABLE tasks(
 	CONSTRAINT task_end_max CHECK (task_end >= 0 AND task_end <= 24)
 );
 
-CREATE TABLE schedule_tasks(
+create TABLE schedule_tasks(
 	task_id integer,
 	schedule_id integer,
 	FOREIGN KEY(task_id) REFERENCES tasks(task_id),
 	FOREIGN KEY(schedule_id) REFERENCES schedules(schedule_id)
 );
 
-CREATE TABLE all_audit(
+create TABLE all_audit(
 	event_counter integer UNIQUE PRIMARY KEY,
 	event_name varchar(100),
 	table_name varchar(100),
-	user_id varchar(40),
+	user_id integer,
 	event_date timestamp
 );
 
-CREATE SEQUENCE counter AS integer INCREMENT BY 1 START 1; 
+create sequence counter AS integer INCREMENT BY 1 START 1;
 
-CREATE OR REPLACE FUNCTION process_audit() RETURNS TRIGGER AS '
+create or replace function process_audit() RETURNS trigger AS '
     BEGIN
         IF (TG_OP = ''DELETE'') THEN
             INSERT INTO all_audit
@@ -72,7 +72,7 @@ CREATE OR REPLACE FUNCTION process_audit() RETURNS TRIGGER AS '
     END;
 ' LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION check_task_id() RETURNS TRIGGER AS '
+create or replace function check_task_id() RETURNS trigger AS '
     BEGIN
         IF (TG_OP = ''INSERT'') THEN
             DECLARE
@@ -89,39 +89,39 @@ CREATE OR REPLACE FUNCTION check_task_id() RETURNS TRIGGER AS '
     END;
 ' LANGUAGE plpgsql;
 
-CREATE TRIGGER tasks_audit_ins
-    AFTER INSERT ON tasks
-    FOR EACH ROW EXECUTE Procedure process_audit();
-CREATE TRIGGER tasks_audit_upd
-    AFTER UPDATE ON tasks
-    FOR EACH ROW EXECUTE procedure process_audit();
-CREATE TRIGGER tasks_audit_del
-    AFTER DELETE ON tasks
-    FOR EACH ROW EXECUTE procedure process_audit();
+create trigger tasks_audit_ins
+    after insert on tasks
+    for each row EXECUTE procedure process_audit();
+create trigger tasks_audit_upd
+    after update on tasks
+    for each row EXECUTE procedure process_audit();
+create trigger tasks_audit_del
+    after delete on tasks
+    for each row EXECUTE procedure process_audit();
 
-CREATE TRIGGER schedules_audit_ins
-    AFTER INSERT ON schedules
-    FOR EACH ROW EXECUTE Procedure process_audit();
-CREATE TRIGGER schedules_audit_upd
-    AFTER UPDATE ON schedules
-    FOR EACH ROW EXECUTE procedure process_audit();
-CREATE TRIGGER schedules_audit_del
-    AFTER DELETE ON schedules
-    FOR EACH ROW EXECUTE procedure process_audit();
+create trigger schedules_audit_ins
+    after insert on schedules
+    for each row EXECUTE procedure process_audit();
+create trigger schedules_audit_upd
+    after update on schedules
+    for each row EXECUTE procedure process_audit();
+create trigger schedules_audit_del
+    after delete on schedules
+    for each row EXECUTE procedure process_audit();
 
-CREATE TRIGGER users_audit_ins
-    AFTER INSERT ON users
-    FOR EACH ROW EXECUTE Procedure process_audit();
-CREATE TRIGGER users_audit_upd
-    AFTER UPDATE ON users
-    FOR EACH ROW EXECUTE procedure process_audit();
-CREATE TRIGGER users_audit_del
-    AFTER DELETE ON users
-    FOR EACH ROW EXECUTE procedure process_audit();
+create trigger users_audit_ins
+    after insert on users
+    for each row EXECUTE procedure process_audit();
+create trigger users_audit_upd
+    after update on users
+    for each row EXECUTE procedure process_audit();
+create trigger users_audit_del
+    after delete on users
+    for each row EXECUTE procedure process_audit();
 
-CREATE TRIGGER schedule_task_check
-    BEFORE INSERT ON schedule_tasks
-    FOR EACH ROW EXECUTE procedure check_task_id();
+create trigger schedule_task_check
+    before insert on schedule_tasks
+    for each row EXECUTE procedure check_task_id();
 
     -- users
     INSERT INTO users(user_name, user_email, user_password, user_role) VALUES('a', 'a', '1000:52a2e5376fe9155814775f1e3231a526:191ade9da2dcbabfc870ba70263b7af6865b40d8e179d19e8ea504d257810c6e78a316d77f5bd8716a7fa54f39b1f082c773ca80b45526dd59c933522e341216', 'ADMIN');
