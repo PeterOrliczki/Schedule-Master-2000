82cda7af113af54f9a8bb0cf7a64b05480b574ff
berta <berta.vincze@gmail.com>
2019-05-21 21:22:31 +0200

    Add doPost & doDelete methods to support the new task adding system

--- src/main/java/com/codecool/web/servlet/TaskServlet.java
+++ src/main/java/com/codecool/web/servlet/TaskServlet.java
@@ -4,11 +4,9 @@ import com.codecool.web.dao.ScheduleDao;
 import com.codecool.web.dao.TaskDao;
 import com.codecool.web.dao.database.DatabaseScheduleDao;
 import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.dto.RelationDto;
 import com.codecool.web.model.Task;
-import com.codecool.web.model.User;
-import com.codecool.web.service.ScheduleService;
 import com.codecool.web.service.TaskService;
-import com.codecool.web.service.simple.SimpleScheduleService;
 import com.codecool.web.service.simple.SimpleTaskService;
 
 import javax.servlet.ServletException;
@@ -18,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
 
 @WebServlet("/protected/task")
@@ -39,5 +36,42 @@ public class TaskServlet extends AbstractServlet {
         }
     }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao, scheduleDao);
+
+            int taskId = Integer.valueOf(request.getParameter("taskId"));
+            int scheduleId = Integer.valueOf(request.getParameter("scheduleId"));
+            int columnNumber = Integer.valueOf(request.getParameter("columnNumber"));
+            if (taskService.doesRelationExistByTaskAndScheduleId(taskId, scheduleId)) {
+                RelationDto relationDto = new RelationDto(taskId, columnNumber, scheduleId);
+                sendMessage(response, HttpServletResponse.SC_OK, relationDto);
+            } else {
+                sendMessage(response, HttpServletResponse.SC_OK, "False");
+            }
+
+        } catch (SQLException exc) {
+            handleSqlError(response, exc);
+        }
+    }
+
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = getConnection(request.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao, scheduleDao);
+
+            int taskId = Integer.valueOf(request.getParameter("taskId"));
+            int scheduleId = Integer.valueOf(request.getParameter("scheduleId"));
+            int columnNumber = Integer.valueOf(request.getParameter("columnNumber"));
+            taskService.deleteRelationRecordByTaskAndScheduleId(taskId, scheduleId);
+            RelationDto relationDto = new RelationDto(taskId, columnNumber, scheduleId);
+            sendMessage(response, HttpServletResponse.SC_OK, relationDto);
+        } catch (SQLException exc) {
+            handleSqlError(response, exc);
+        }
+    }
 
 }
