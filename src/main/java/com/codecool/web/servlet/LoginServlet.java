b8de013cb42a96cb646686cdc3317833d7f9332e
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-22 07:44:09 +0200

    started working on logging

--- src/main/java/com/codecool/web/servlet/LoginServlet.java
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -6,6 +6,8 @@ import com.codecool.web.model.User;
 import com.codecool.web.service.UserService;
 import com.codecool.web.service.exception.ServiceException;
 import com.codecool.web.service.simple.SimpleUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +19,8 @@ import java.sql.SQLException;
 @WebServlet("/login")
 public final class LoginServlet extends AbstractServlet {
 
+    private static Logger logger = LoggerFactory.getLogger(LoginServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
@@ -26,8 +30,10 @@ public final class LoginServlet extends AbstractServlet {
             String password = req.getParameter("password");
             User user = userService.loginUser(email, password);
             req.getSession().setAttribute("user", user);
+            logger.info(user.getEmail());
             sendMessage(resp, HttpServletResponse.SC_OK, user);
         } catch (ServiceException ex) {
+            logger.error(ex.getMessage());
            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
         } catch (SQLException ex) {
             handleSqlError(resp, ex);
