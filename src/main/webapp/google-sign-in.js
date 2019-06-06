41eb83ee5d54a1cb1e274b2cbfe0e17e8c6d4eec
AndreaSzoboszlai <szobandi@gmail.com>
2019-06-06 09:12:26 +0200

    Google sign out works now

--- src/main/webapp/google-sign-in.js
+++ src/main/webapp/google-sign-in.js
@@ -26,6 +26,6 @@ function onVerified() {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+      onLogoutClicked();
     });
 }
